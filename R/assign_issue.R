78d58543257b54b8c6f2e1793365e9d39f07a5f1
Derek Chiu <dchiu@bccrc.ca>
2022-12-05 14:02:57 -0800

    add assign_issue

---
+++ R/assign_issue.R
@@ -0,0 +1,26 @@
+#' Assigns an issue to a user
+#'
+#' Assign an issue to a user for a JIRA ticket
+#'
+#' @param name username to assign
+#' @inheritParams get_comment
+#' @author Derek Chiu
+#' @export
+assign_issue <- function(name, issue = NULL) {
+  issuekey <- issue %||% basename(here::here())
+  res <- httr::PUT(
+    url = paste(BASE_URL, issuekey, "assignee", sep = "/"),
+    config = httr::authenticate(
+      user = keyring::key_list(service = "jira")[["username"]],
+      password = keyring::key_get(service = "jira")
+    ),
+    body = list(name = name),
+    encode = "json",
+    httr::add_headers("X-Atlassian-Token" = "no-check")
+  )
+  if (httr::http_error(res)) {
+    httr::stop_for_status(res)
+  } else {
+    paste0("Issue ", issue, " assigned to user ", name)
+  }
+}
