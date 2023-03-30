0b43595f7e2d6db3c7d04452931a5ab7d65ef384
Derek Chiu <dchiu@bccrc.ca>
2023-03-30 15:08:46 -0700

    add set_auth() internal fun, rename authenticate_jira() to store_auth()

---
+++ R/set_auth.R
@@ -0,0 +1,20 @@
+#' Set configuration settings for JIRA authentication
+#' @noRd
+set_auth <- function(service = "jira") {
+  key <- keyring::key_list(service = service)
+  if (nrow(key) == 0) {
+    stop(
+      paste0(
+        "JIRA credentials not stored in service ",
+        dQuote(service),
+        ". Run `authenticate_jira(username)`."
+      )
+    )
+  }
+  username <- key[["username"]]
+  config <- httr::authenticate(
+    user = username,
+    password = keyring::key_get(service = service, username = username)
+  )
+  invisible(config)
+}
