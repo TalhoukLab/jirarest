451be6e757b319ed8153dce6a8c6177063d90430
Derek Chiu <dchiu@bccrc.ca>
2023-05-25 16:00:48 -0700

    new helper fun get_epic_linked_issues()

---
+++ R/get_epic_linked_issues.R
@@ -0,0 +1,21 @@
+#' Returns linked issues to an Epic
+#'
+#' Return all linked issues to an Epic issue.
+#'
+#' @inheritParams get_comment
+#' @author Derek Chiu
+#' @export
+get_epic_linked_issues <- function(issue = NULL) {
+  issuekey <- issue %||% basename(here::here())
+  res <- httr::GET(
+    url = paste0("https://www.bcgsc.ca/jira/rest/api/2/",
+                 "search?jql='Epic Link'=",
+                 issuekey),
+    config = set_auth("jira"),
+    encode = "json",
+    httr::add_headers("X-Atlassian-Token" = "no-check")
+  )
+  cont <- httr::content(res)
+  linked_issues <- sort(purrr::map_chr(cont[["issues"]], "key"))
+  linked_issues
+}
