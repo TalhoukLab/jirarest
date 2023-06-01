6cfbbc23d67e6879b96fc1a2b491acfb518b3012
Derek Chiu <dchiu@bccrc.ca>
2023-05-31 17:40:16 -0700

    tmp update without using httr

--- R/set_auth.R
+++ R/set_auth.R
@@ -18,3 +18,19 @@ set_auth <- function(service = "jira") {
   )
   invisible(config)
 }
+
+set_auth2 <- function(service = "jira") {
+  key <- keyring::key_list(service = service)
+  if (nrow(key) == 0) {
+    stop(
+      paste0(
+        "JIRA credentials not stored in service ",
+        dQuote(service),
+        ". Run `authenticate_jira(username)`."
+      )
+    )
+  }
+  username <- key[["username"]]
+  password <- keyring::key_get(service = service, username = username)
+  invisible(list(username = username, password = password))
+}
