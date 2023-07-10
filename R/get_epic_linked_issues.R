81c5998f63f047623a432e974eeb458c0c483ab6
Derek Chiu <dchiu@bccrc.ca>
2023-07-10 15:31:42 -0700

    refactor get_epic_linked_issues() using httr2

--- R/get_epic_linked_issues.R
+++ R/get_epic_linked_issues.R
@@ -7,14 +7,15 @@
 #' @export
 get_epic_linked_issues <- function(issue = NULL) {
   issuekey <- issue %||% basename(here::here())
-  res <- httr::GET(
-    url = paste0(BASE_URL, "/search?jql='Epic Link'=", issuekey),
-    config = set_auth("jira"),
-    encode = "json",
-    httr::add_headers("X-Atlassian-Token" = "no-check")
-  )
-  cont <- httr::content(res)
+  req <- httr2::request(BASE_URL) %>%
+    httr2::req_url_path_append("search") %>%
+    httr2::req_url_query(jql = paste0("'Epic Link'=", issuekey)) %>%
+    rlang::list2(!!!set_auth2("jira")) %>%
+    rlang::exec(httr2::req_auth_basic, !!!.)
+  resp <- req %>%
+    httr2::req_perform() %>%
+    httr2::resp_body_json()
   linked_issues <-
-    sort(vapply(cont[["issues"]], function(x) x[["key"]], character(1)))
+    sort(vapply(resp[["issues"]], function(x) x[["key"]], character(1)))
   linked_issues
 }
