f93e9c1e1a44564592671097479442ed07df4577
Derek Chiu <dchiu@bccrc.ca>
2026-03-18 17:03:34 -0700

    add get_issue_assignee()

---
+++ R/get_issue_assignee.R
@@ -0,0 +1,19 @@
+#' Returns assignee for an issue
+#'
+#' Returns the user designated as the assignee for an issue
+#'
+#' @inheritParams get_comment
+#' @author Derek Chiu
+#' @export
+get_issue_assignee <- function(issue = NULL) {
+  issuekey <- issue %||% basename(here::here())
+  req <-
+    httr2::request(base_url = paste(BASE_URL, "issue", issuekey, sep = "/")) %>%
+    rlang::list2(!!!set_auth("jira")) %>%
+    rlang::exec(httr2::req_auth_basic, !!!.)
+  resp <- req %>%
+    httr2::req_perform() %>%
+    httr2::resp_body_json()
+  assignee <- purrr::pluck(resp, "fields", "assignee", "displayName")
+  if (is.null(assignee)) "Unassigned" else assignee
+}
